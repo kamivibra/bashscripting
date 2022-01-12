8815aa88e0ff5c9e1d66298088dedd4b3baa5488
kamivibra <kami_vibra@yahoo.com>
2022-01-11 23:36:50 -0500

    adding a file and some editing for student practice only

---
+++ first.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "This is created for the first time to teach my students"
+
+echo "This changed is made through console, file is opened from desktop"
+
---
+++ zayaan.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+numbers="1 2 3 4 5 6 7 8 9 10"
+NAME="Kamran "          #variable
+echo "My Name is "${NAME}Khan        # Concatenating and calling a variable
+echo $numbers
